7b41b3c7467f75cd4f580fc3a70339ee14b9b570
05xapresses <40399432+05xapresses@users.noreply.github.com>
2020-06-22 15:07:59 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//btw juby is cute
+const { Plugin } = require('powercord/entities')
+const { get } = require('powercord/http')
+
+module.exports = class TextReact extends Plugin {
+    startPlugin() {
+
+        powercord.api.commands.registerCommand({
+            aliases: ["kurwapogoda"],
+            command: 'weather',
+            description: 'checks weather',
+            usage: '{c} [city/any name location/airport code/domain/area code/GPS coordinates]',
+            executor: async args => {
+                const req = await get('https://wttr.in/' + args.join('+')).query('0T', '').query('force-ansi', '1')
+                if (req.statusCode != 200) return { result: 'something went wrong™️' }
+                
+                let result = { result: "```\n" + req.body.toString() + "\n```" }
+                return result 
+            }
+        })
+    }
+
+    pluginWillUnload() {
+        powercord.api.commands.unregisterCommand('weather')
+    }
+}
