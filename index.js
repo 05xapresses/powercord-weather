cc5d7ad33dcb5811b82cd01cf2c0a112f51a8d66
05xapresses <40399432+05xapresses@users.noreply.github.com>
2020-06-27 11:17:40 +0200

    settings

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 //btw juby is cute
 const { Plugin } = require('powercord/entities')
 const { get } = require('powercord/http')
+const Settings = require('./Settings')
 
 module.exports = class TextReact extends Plugin {
     startPlugin() {
+
+        powercord.api.settings.registerSettings('weather', {
+            category: this.entityID,
+            label: 'Weather',
+            render: Settings
+        })
+
 
         powercord.api.commands.registerCommand({
             aliases: ["kurwapogoda", "jebacpis"],
@@ -11,16 +19,31 @@ module.exports = class TextReact extends Plugin {
             description: 'checks weather',
             usage: '{c} [city/any name location/airport code/domain/area code/GPS coordinates]',
             executor: async args => {
-                const req = await get('https://wttr.in/' + encodeURIComponent(args.join(' '))).query('0T', '').query('force-ansi', '1')
+                let location
+                let req
+
+                if (args.join(" ") == ""){
+                    location = this.settings.get('location','')
+                } else {
+                    location = args.join(" ")
+                }
+
+                if (this.settings.get('asciiart', 'true') == false){
+                    req = await get('https://wttr.in/' + encodeURIComponent(location)).query((this.settings.get('units','m')), '').query('format',(this.settings.get('nonasciidisplay',4))).query('force-ansi', '1')
+                } else {
+                    req = await get('https://wttr.in/' + encodeURIComponent(location)).query('0T' + (this.settings.get('units','m')), '').query('force-ansi', '1')
+                }
+
                 if (req.statusCode != 200) return { result: 'something went wrong™️' }
                 
                 let result = { result: "```\n" + req.body.toString() + "\n```" }
-                return result 
+                return result
             }
         })
     }
 
     pluginWillUnload() {
+        powercord.api.settings.unregisterSettings('weather')
         powercord.api.commands.unregisterCommand('weather')
     }
 }
