48ddc6c13422db422004c1b4369b397dcc105ea2
pxseu <57842793+pxseu@users.noreply.github.com>
2021-01-15 17:21:33 +0100

    web commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//btw juby is cute
+//btw juby is even cuter <3333
 const { Plugin } = require('powercord/entities')
 const { get } = require('powercord/http')
 const Settings = require('./Settings')
@@ -23,13 +23,13 @@ module.exports = class Weather extends Plugin {
                 let location
                 let req
 
-                if (args.join(" ") == ""){
+                if (!args.join(" ")){
                     location = this.settings.get('location','')
                 } else {
                     location = args.join(" ")
                 }
 
-                if (this.settings.get('asciiart', true) == false){
+                if (!this.settings.get('asciiart', true)){
                     req = await get('https://wttr.in/' + encodeURIComponent(location)).query((this.settings.get('units','m')), '').query('format',(this.settings.get('nonasciidisplay',4))).query('force-ansi', '1')
                 } else {
                     req = await get('https://wttr.in/' + encodeURIComponent(location)).query('0T' + (this.settings.get('units','m')), '').query('force-ansi', '1')
@@ -37,7 +37,7 @@ module.exports = class Weather extends Plugin {
 
                 if (req.statusCode != 200) return { result: 'something went wrong™️' }
                 
-                let result = { result: "```\n" + req.body.toString() + "\n```" }
+                let result = { result: `\`\`\`\n${req.body.toString()}\n\`\`\`` }
                 return result
             }
         })
